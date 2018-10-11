d5f1b33f785bc14dca8c35a22b63931991d90f48
이예현 <shszyh@iclooud.com>
2018-10-11 15:34:55 +0900

    02

--- if else_1.c
+++ if else_1.c
@@ -7,14 +7,10 @@ int main(int argc, char *argv[]) {
 	int a;
 	printf("정수하나를 입력하시오:");
 	scanf("%d",&a);
-	if(a>0)
-		printf("양수입니다.\n");
-	else if(a==0)
-		printf("0입니다.\n");
-	else 
-		printf("음수입니다.\n");
-	
-	
+	if(a>=0)
+		printf("절대값은 %d 입니다.\n",a);
+	else
+		printf("절대값은 %d 입니다.\n",-a);
 	
 	
 	return 0;
